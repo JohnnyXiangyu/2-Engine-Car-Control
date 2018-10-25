3068c09da5d4ea88039b0aa6c702b48eacf5551e
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2018-10-25 15:28:38 -0700

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -8,32 +8,48 @@ import Keyboard as keyboard
 #
 #还有其他组件需要加在这
 
-def main():
-    """主循环"""
-    pygame.init()
-    screen = pygame.display.set_mode((800,600))
-    pygame.display.set_caption('Console')
-
-    #初始化电机对象
-    engine1 = engine.Engine(1)
-    engine2 = engine.Engine(2)
-
-    while True:
-        check_event(engine1, engine2)
-        engine.flip(engine1, engine2)
+def init():
+    """初始化"""
+    #初始化电机(一个引擎一个舵机)对象，设置状态为停止，并提供报错通道
+    try:
+        main_engine = engine.Engine(0)
+        steer = engine.Engine(0)
 
 
-def check_event(engine1, engine2):
+    except:
+        print('ENGINE INITIALIZATION FAILED')
+        input('PRESS ENTER TO QUIT')
+        sys.exit()
+
+    #启动pygame界面
+    try:
+        pygame.init()
+        screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption('Console')
+    except:
+        print('GRAPHIC CONSOLE INITIALIZATION FAILED')
+        input('PRESS ENTER TO QUIT')
+        sys.exit()
+       
+
+
+def check_event(engine, steer):
     """键盘事件检查"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            keyboard.action_down(event.key, engine1, engine2)
+            keyboard.action_down(event.key, engine, steer)
 
         elif event.type == pygame.KEYUP:
-            keyboard.action_up(event.key, engine1, engine2)
+            keyboard.action_up(event.key, engine, steer)
 
 
-main()
+    
+init()
+            
+#开启循环
+while True:
+    check_event(main_engine, steer)
+    engine.flip(main_engine, steer)

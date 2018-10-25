f18b8c1f7492b634355737460d2ce8473770b9ca
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2018-10-25 15:29:09 -0700

    Update Keyboard.py

--- Keyboard.py
+++ Keyboard.py
@@ -1,35 +1,15 @@
 import pygame 
+import Engines as engines
 
-def action_down(key, engine1, engine2):
+def action_down(key, engine, steer):
     """响应键盘按下"""
     if key == pygame.K_w:
-        engine1.direction = 1
-        engine1.speed = 1
-        engine2.direction = 1
-        engine2.speed = 1
-
-    elif key == pygame.K_a:
-        engine1.direction = -1
-        engine1.speed = 1
-        engine2.direction = 1
-        engine2.speed = 1
+        engine.status = 1
 
     elif key == pygame.K_s:
-        engine1.direction = -1
-        engine1.speed = 1
-        engine2.direction = -1
-        engine2.speed = 1
-
-    elif key == pygame.K_d:
-        engine1.direction = 1
-        engine1.speed = 1
-        engine2.direction = -1
-        engine2.speed = 1
+        engine.status = 2
 
 
 def action_up(key, engine1, engine2):
     """响应键盘抬起"""
-    engine1.direction = 1
-    engine1.speed = 0
-    engine2.direction = 1
-    engine2.speed = 0
+    
